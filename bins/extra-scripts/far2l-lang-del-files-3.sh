960d77b6256e5d65da58fa6e765c3fb06b48179c
deep-soft <deep-soft@users.noreply.github.com>
2026-03-15 12:34:36 +0200

    Update far2l-lang-del-files-3.sh

--- bins/extra-scripts/far2l-lang-del-files-3.sh
+++ bins/extra-scripts/far2l-lang-del-files-3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # far2l-lang-del-files-3.sh
 
-lng_hlf_files=$(find . -type f \( -name "*.hlf" -o -name "*.lng" \) | grep -E -i -v "(eng|en|ce|re|pe|he).(lng|hlf)");
+lng_hlf_files=$(find . -type f \( -name "*.hlf" -o -name "*.lng" \) | grep -E -i -v "(eng|en|ce|re|pe|he|memoe).(lng|hlf)");
 #size=${#lng_hlf_files}; echo $size;
 max_line_len=1; for lng_hlf_file in $lng_hlf_files; do  line_len=${#lng_hlf_file};  if [[ $line_len -gt $max_line_len ]]; then    max_line_len=$line_len;  fi; done; max_line_len=$max_line_len+1;
 
